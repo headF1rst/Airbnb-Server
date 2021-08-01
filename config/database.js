707b187f606351d8f773c9dd2780f275461e4660
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2021-08-01 18:24:07 +0900

    dev/prod 분리

--- config/database.js
+++ config/database.js
@@ -1,14 +1,27 @@
 const mysql = require('mysql2/promise');
 const {logger} = require('./winston');
-
+let pool;
 // TODO: 본인의 DB 계정 입력
-const pool = mysql.createPool({
-    host: '',
-    user: '',
-    port: '3306',
-    password: '',
-    database: ''
-});
+if(process.env.NODE_ENV == 'development')
+{
+    pool = mysql.createPool({
+        host: 'database-1.c9v46leqnkb2.ap-northeast-2.rds.amazonaws.com',
+        user: 'admin',
+        port: '3306',
+        password: 'Kershaw22!',
+        database: 'airbnbDB-dev'
+    });
+}
+else if(process.env.NODE_ENV == 'production')
+{
+    pool = mysql.createPool({
+        host: 'database-1.c9v46leqnkb2.ap-northeast-2.rds.amazonaws.com',
+        user: 'admin',
+        port: '3306',
+        password: 'Kershaw22!',
+        database: 'airbnbDB-prod'
+    });
+}
 
 module.exports = {
     pool: pool
